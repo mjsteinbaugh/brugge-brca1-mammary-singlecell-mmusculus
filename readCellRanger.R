7bc5354fc30f86056a70f3f92b20b87aa87ebfbb
Michael Steinbaugh <mike@steinbaugh.com>
2018-05-31 12:29:02 -0400

    Update load script

--- readCellRanger.R
+++ readCellRanger.R
@@ -1,8 +1,8 @@
 library(bcbioSingleCell)
-bcb <- loadCellRanger(
-    uploadDir = "data/cellranger",
+bcb <- readCellRanger(
+    uploadDir = "cellranger",
     refDataDir = "annotations/refdata-cellranger-mm10-1.2.0",
     sampleMetadataFile = "meta/sample_metadata.xlsx",
     interestingGroups = "genotype"
 )
-saveData(bcb, dir = "data")
+saveData(bcb, dir = file.path("data", Sys.Date()))
